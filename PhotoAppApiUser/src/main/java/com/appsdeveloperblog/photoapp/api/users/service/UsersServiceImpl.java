2e0433ac92fc2f7c631c94602851e7116f5c2b25
Cristiano Gomes <cristianno@ymail.com>
2020-05-11 21:50:31 -0300

    Adiciona cadastro de usuário e spring security

---
+++ PhotoAppApiUser/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UsersServiceImpl.java
@@ -0,0 +1,44 @@
+package com.appsdeveloperblog.photoapp.api.users.service;
+
+import com.appsdeveloperblog.photoapp.api.users.data.UserEntity;
+import com.appsdeveloperblog.photoapp.api.users.data.UsersRepository;
+import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+
+    private UsersRepository usersRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UsersServiceImpl(UsersRepository usersRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.usersRepository = usersRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    public UserDto createUser(UserDto userDetails) {
+        userDetails.setUserId(UUID.randomUUID().toString());
+        userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
+
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
+
+        usersRepository.save(userEntity);
+
+        UserDto returnValue = modelMapper.map(userEntity, UserDto.class);
+
+        return returnValue;
+    }
+
+}
--- PhotoAppApiUser/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UserController.java
+++ PhotoAppApiUser/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UserController.java
@@ -1,10 +1,18 @@
 package com.appsdeveloperblog.photoapp.api.users.ui.controllers;
 
+import com.appsdeveloperblog.photoapp.api.users.service.UsersService;
+import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
+import com.appsdeveloperblog.photoapp.api.users.ui.model.CreateUserRequestModel;
+import com.appsdeveloperblog.photoapp.api.users.ui.model.CreateUserResponseModel;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
@@ -13,8 +21,25 @@ public class UserController {
     @Autowired
     private Environment env;
 
+    @Autowired
+    private UsersService usersService;
+
     @GetMapping("/status/check")
     public String status() {
-        return "Working on port" + env.getProperty("local.server.port");
+        return "Working on port " + env.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+
+        UserDto createdUserDto = usersService.createUser(userDto);
+
+        CreateUserResponseModel returnValue = modelMapper.map(createdUserDto, CreateUserResponseModel.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
     }
 }
