3131211d0655c2bcb32307a714a6c0097abb2862
Cristiano Gomes <cristianno@ymail.com>
2020-05-16 12:03:30 -0300

    Configura autenticação na API Gateway

---
+++ PhotoAppApiZuulApiGateway/src/main/java/com/appsdeveloperblog/photoapp/api/gateway/security/WebSecurity.java
@@ -0,0 +1,34 @@
+package com.appsdeveloperblog.photoapp.api.gateway.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    private Environment environment;
+
+    public WebSecurity(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http.authorizeRequests()
+            .antMatchers(environment.getProperty("api.url.path.h2console")).permitAll()
+            .antMatchers(HttpMethod.POST, environment.getProperty("api.url.path.registration")).permitAll()
+            .antMatchers(HttpMethod.POST, environment.getProperty("api.url.path.login")).permitAll()
+            .anyRequest().authenticated();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+}
