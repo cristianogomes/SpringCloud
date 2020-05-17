0b53767a79dc7df82f3a831fc43bcb7f60747235
Cristiano Gomes <cristianno@ymail.com>
2020-05-17 12:36:36 -0300

    Inicia projeto PhotoAppApiConfigServer

---
+++ PhotoAppApiConfigServer/src/test/java/com/appsdeveloperblog/photoapp/api/PhotoAppApiConfigServer/PhotoAppApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.api.PhotoAppApiConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
